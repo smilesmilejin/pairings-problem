36be955ba8ccadaec07040f64b0388cbe070bdc5
Xinshuang Jin <jinxinshuang@gmail.com>
2025-06-13 14:28:01 -0400

    2025/06/13 Interview Prep

--- pairings.py
+++ pairings.py
@@ -1,6 +1,42 @@
 def find_pairs(num_string):
   # Write your solution here!
-  pass
+  # pass
+  # duplicate? no duplicate
+  # match with itselt?
+
+  # split each num in the numstring into a list
+  # convert the list into list of integers
+
+  # create a empty set
+  # find all possible pairs in num_string
+  # add (min, max) to empty set
+
+  # return result
+
+  num_string_split = num_string.split()
+
+  nums = [int(num)for num in num_string_split]
+  # print(nums)
+
+  result = set()
+
+  for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      num1 = nums[i]
+      num2 = nums[j]
+
+      if num1 < num2:
+        pair = (num1, num2)
+      elif num1 > num2:
+        pair = (num2, num1)
+
+      result.add(pair)
+
+  # print(result)
+  return result
+      
+
+
 
 
 # Test cases
